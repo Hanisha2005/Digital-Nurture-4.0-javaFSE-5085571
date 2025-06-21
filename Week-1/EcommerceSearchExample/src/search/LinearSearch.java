eacb68cc5f47a66b89ed801f677ad0538e6496ca
Hanisha2005 <hanishatavva@gmail.com>
2025-06-21 10:03:37 +0530

    Add solutions for practice problems

---
+++ Week-1/EcommerceSearchExample/src/search/LinearSearch.java
@@ -0,0 +1,14 @@
+package search;
+
+import model.Product;
+
+public class LinearSearch {
+    public static Product searchByName(Product[] products, String name) {
+        for (Product product : products) {
+            if (product.getProductName().equalsIgnoreCase(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
